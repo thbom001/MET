ceb22d88afd7b55fe0f13d83fdc2153ebed75bcc
Julie Prestopnik <jpresto@ucar.edu>
2025-04-01 10:26:39 -0600

    Feature METplus2771 main_v12.0 subprojects (#3116)
    * Adding ToC for subprojects
    
    * Removing references to feature branch

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'Flowchart' ]
 suppress_warnings = ['ref.citation']
 
 # -- Options for HTML output -------------------------------------------------
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
