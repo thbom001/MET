c252a5425d33c191d6e004b504063dc9036d71e1
Tim Hume <timothy.hume@bom.gov.au>
2025-08-08 10:36:15 +1000

    More changes.

--- src/tools/test_vx_grid/test_acea.cc
+++ src/tools/test_vx_grid/test_acea.cc
@@ -16,7 +16,8 @@ using namespace std;
 int main(){
 	std::cout << "Hello World!\n";
 	
-	Grid g;
+	Grid *g = (Grid *) nullptr;
+	std::cout << g->nx();
 	AlbersData adata;
 	adata.std_parallel_1 = -36.0;
 	adata.std_parallel_2 = -18.0;
@@ -29,6 +30,8 @@ int main(){
 	adata.dx_m				= 2400.;
 	adata.dy_m				= 2400.;
 	adata.eccentricity	= 0.0;
+
+	g = new Grid ( adata );
 
 	double lat, lon;
 	//adata.xy_to_latlon(-100, -3000000, &lat, &lon);
