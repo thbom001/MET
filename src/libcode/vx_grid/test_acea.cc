9dcce01a6c2b6889258c4f6dc40e5461b802e568
Tim Hume <timothy.hume@bom.gov.au>
2025-08-08 10:36:14 +1000

    Test the Albers projection.

---
+++ src/libcode/vx_grid/test_acea.cc
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cmath>
+
+#include "vx_math.h"
+#include "vx_util.h"
+#include "vx_log.h"
+#include "acea_grid.h"
+
+
+using namespace std;
+
+int main(){
+	std::cout << "Hello World!";
+	return 0
+}
